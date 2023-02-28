6ca72568167c20fb0767d9f2ac2c8f5062c5e175
Eliud Gateri <egateri@gmail.com>
2023-02-28 18:29:56 +0300

    Create the Project

---
+++ README.md
@@ -0,0 +1,34 @@
+## Project Name
+JavaScript Program Grade Exam Scores
+## Author
+ - Name : Eliud Gateri
+ - Email: egateri@gmail.com
+ - Role : Student at Moringa School
+
+## About this project
+This project is aimed at demostrating how to use JavaScript to grade exam scores
+
+## Instructions to use this project
+Access the site on the live link to view the deployed project.
+
+To contribute to this project:
+1. Request to contribute
+1. Clone the project
+1. Create dev branch, develop and test
+1. Review and request to merge the master branch
+
+## Live link
+https://egateri.github.io/score
+
+## Technologies and Languages used
+ * Technologies
+ 1. Git Bash
+ 1. VS code
+ 1. Git Hub
+ * Languages
+ 1. HTML : HyperText Makeup language
+ 1. Java Script
+ 1. Markdown
+
+## License
+(c) 2023 Eliud Gateri
---
+++ script.js
@@ -0,0 +1,36 @@
+function gradeFunction() {
+    var x = document.getElementById("formScore").elements[0].value;
+  var score = parseInt(x);
+  //  score = parseInt(prompt("Enter your score"));
+  if (score >= 80 && score <=100) {
+      //console.log("You scored an A");
+     
+      document.getElementById("demo").innerHTML = "You scored an A";
+  }
+  else if (score < 80 && score >= 60 ) {
+      // console.log("You scored an B");
+     
+       document.getElementById("demo").innerHTML = "You scored an B";
+  }
+    else if (score < 60 && score >= 40 ) {
+       //console.log("You scored an C");
+      
+       document.getElementById("demo").innerHTML = "You scored an C";
+  }
+        else if (score < 40 && score >= 20 ) {
+       //console.log("You scored an D");
+       
+       document.getElementById("demo").innerHTML = "You scored an D";
+  }
+      else if (score < 20 && score >= 0 ) {
+      // console.log("You scored an E");
+      
+       document.getElementById("demo").innerHTML = "You scored an E";
+  }  
+  
+  else {
+ 
+    document.getElementById("demo").innerHTML = "Error:Please input values between 0 and 100";
+  }
+}
+ 
